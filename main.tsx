1a8c454a4eba41f57dd9a3e9fe7653ae97001b2c
Botuvo25 <iqbal@botuvo.co.uk>
2025-07-23 03:43:45 +0100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,8 +1,8 @@
-console.log('main.tsx loaded');
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+
+console.log('main.tsx loaded'); // optional, for debugging
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
