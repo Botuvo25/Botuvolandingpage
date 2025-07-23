b494a3dd1240f26c0fb486a42ad69b43d9c896bc
Botuvo25 <iqbal@botuvo.co.uk>
2025-07-23 03:04:09 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import IndexPage from "./pages/Index";       // import directly from Index.tsx
+import NotFoundPage from "./pages/NotFound"; // import directly from NotFound.tsx
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
