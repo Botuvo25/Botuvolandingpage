abc125893183680627a5f9af05302133ea2416c9
Botuvo25 <iqbal@botuvo.co.uk>
2025-07-24 10:49:59 +0100

    Update Index.tsx

--- Index.tsx
+++ Index.tsx
@@ -1,10 +1,15 @@
 console.log('Index.tsx rendered');
 
-const Index = () => (
-  <div style={{ padding: '20px', backgroundColor: '#eee' }}>
-    <h1>Test Content</h1>
-    <p>If you see this, your React app is rendering.</p>
-  </div>
-);
+import Hero from "@/components/Hero";
+// ... other imports
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      {/* other components */}
+    </div>
+  );
+};
 
 export default Index;
